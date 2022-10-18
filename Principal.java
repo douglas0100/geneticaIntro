82981b092c2ae5b59ae478fbf556fc4b0426d9e4
douglas0100 <douglasfelipe0100@gmail.com>
2022-10-18 14:09:26 +0000

    adiciona metodo de cruzamento na main

--- Principal.java
+++ Principal.java
@@ -1,12 +1,20 @@
 import java.util.LinkedList;
 import java.util.List;
 
+import Algoritmos.Cruzamento;
 import Individuo.Individuo;
 
 public class Principal {
     public static void main(String[] args) {
         
         List<Individuo> populacao = criaPopulacao(10);
+        imprimePopulacao(populacao);
+
+        cruzaPopulacao(populacao);
+
+        System.out.println("");
+        System.out.println("");
+
         imprimePopulacao(populacao);
         
     }
@@ -18,6 +26,28 @@ public class Principal {
             populacao.add(novoIndividuo);
         }
         return populacao;
+    }
+
+    public static void cruzaPopulacao(List<Individuo> populacao){
+
+        int qtdIndividuos = populacao.size();
+
+        for (int i = 1; i <= qtdIndividuos/2; i++) {
+            Individuo individuo01 = populacao.get(i - 1);
+            Individuo individuo02 = populacao.get(qtdIndividuos - i);
+
+            Cruzamento cruzamento = new Cruzamento(individuo01, individuo02);
+
+            Individuo individuoCruzado01 = cruzamento.criaIndividuoPorCruzamento()[0];
+            Individuo individuoCruzado02 = cruzamento.criaIndividuoPorCruzamento()[1];
+
+            populacao.remove(i - 1);
+            populacao.add(i - 1, individuoCruzado01);
+
+            populacao.remove(qtdIndividuos - i);
+            populacao.add(qtdIndividuos - i, individuoCruzado02);
+            
+        }
     }
 
 
